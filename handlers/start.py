66b6f3197495e61d173051bee8ffbd198e403787
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-04-26 19:45:14 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -37,9 +37,6 @@ Note:- Add @LoverMusicBot and @LoverMusicAssistant to your group and make an adm
                     InlineKeyboardButton(
                         "➕ LoverNetwork ➕", url="https://t.me/LoverNetwork"
                     )],
-                    InlineKeyboardButton(
-                        "💬 Quiz Group", url="https://t.me/CompetitionLevelQuizes"
-                    )],
             ]
         ),
      disable_web_page_preview=True
