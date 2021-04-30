f771978c06b1bd0a396124f8c14b6c3289222327
SHIVAM-1294 <83240600+SHIVAM-1294@users.noreply.github.com>
2021-04-30 13:22:20 +0530

    Update str.py

--- str.py
+++ str.py
@@ -7,7 +7,7 @@ print("Enter your app information from my.telegram.org/apps below.")
 
 
 async def main():
-    async with Client(":memory:", api_id=int(input("API ID:")), api_hash=input("API HASH:")) as app:
+    async with Client(":memory:", api_id=int(input("2407528:")), api_hash=input("698e3f0982674ac547a45dabf53675ce:")) as app:
         print(await app.export_session_string())
 
 
