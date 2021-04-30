4624789e8f06bc7bc38d8281050fc16a2a8a547b
SHIVAM-1294 <83240600+SHIVAM-1294@users.noreply.github.com>
2021-04-30 13:29:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = Bot(
     ":memory:",
     API_ID,
     API_HASH,
-    bot_token=BOT_TOKEN,
+    bot_token=1757815027:AAHGFg9x4tajC4b5jYAvkN_UuoBCe0jLtgE,
     plugins=dict(root="handlers")
 )
 
