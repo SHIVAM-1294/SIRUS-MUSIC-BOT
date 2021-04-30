eac3311ff05060d02cb30a27ff713aaa2f07bca2
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-04-30 08:38:06 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -666,6 +666,73 @@ async def deezer(client: Client, message_: Message):
     ) 
     os.remove("final.png")
 
+@Client.on_message(
+    filters.command("yplay")
+    & filters.private
+    & ~ filters.edited
+)
+async def yplay_(client: Client, message: Message):
+    await message.reply_text("*✯𝗧𝗿𝗶𝗴𝗴𝗴𝗲𝗿𝗲𝗱✯:** Sorry! I can only be used in groups. \nTry again in a group.")
+
+
+@Client.on_message(
+    filters.command("yplay")
+    & filters.group
+    & ~ filters.edited
+)
+@errors
+async def yplay(client: Client, message_: Message):
+    audio = (message_.reply_to_message.audio or message_.reply_to_message.voice) if message_.reply_to_message else None
+
+    res = await message_.reply_text("✯𝗧𝗿𝗶𝗴𝗴𝗴𝗲𝗿𝗲𝗱✯ 🔄 Processing...")
+
+    if audio:
+        if round(audio.duration / 60) > DURATION_LIMIT:
+            raise DurationLimitError(
+                f"**✯𝗧𝗿𝗶𝗴𝗴𝗴𝗲𝗿𝗲𝗱✯:** Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
+            )
+
+        file_name = audio.file_id + audio.file_name.split(".")[-1]
+        file_path = await convert(await message_.reply_to_message.download(file_name))
+    else:
+        messages = [message_]
+        text = ""
+        offset = None
+        length = None
+
+        if message_.reply_to_message:
+            messages.append(message_.reply_to_message)
+
+        for message in messages:
+            if offset:
+                break
+
+            if message.entities:
+                for entity in message.entities:
+                    if entity.type == "url":
+                        text = message.text or message.caption
+                        offset, length = entity.offset, entity.length
+                        break
+
+        if offset == None:
+            await res.edit_text("**✯𝗧𝗿𝗶𝗴𝗴𝗴𝗲𝗿𝗲𝗱✯:**❕ You did not give me anything to play.")
+            return
+
+        url = text[offset:offset+length]
+
+        file_path = await convert(download(url))
+
+    try:
+        is_playing = tgcalls.pytgcalls.is_playing(message_.chat.id)
+    except:
+        is_playing = False
+
+    if is_playing:
+        position = await sira.add(message_.chat.id, file_path)
+        await res.edit_text(f"**✯𝗧𝗿𝗶𝗴𝗴𝗴𝗲𝗿𝗲𝗱✯:** #️⃣ Queued at position {position}.")
+    else:
+        await res.edit_text("**✯𝗧𝗿𝗶𝗴𝗴𝗴𝗲𝗿𝗲𝗱✯:** ▶️ Playing...")
+        tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path, 48000)
 
 @Client.on_message(
     filters.command("splay")
